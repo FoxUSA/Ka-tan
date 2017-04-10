3295851bd9509fa253dbe446f0a636ecbf18071f
FoxUSA <jake@jakelis.com>
2017-04-09 20:21:33 -0700

    Deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,26 @@
 module.exports = function(grunt) {
 	//Initializing the configuration object
 	    grunt.initConfig({
+			compress: {
+				deploy: {
+					options: {
+						archive: "build/version.zip"
+					},
+					files: [{
+						src: ["app/**/*.js",
+							  "app/resources/**/*",
+						  	  "app/app.css",
+						  	  "bower_components/**/*",
+					          "index.html"],
+						expand: true
+					}]
+				}
+			},
 			typescript:{
                 base:{
                     src: [	"bower_components/phaser/typescript/pixi.d.ts",//This info is in 2 places. Here and in the tsconfig. TODO
 				        	"bower_components/phaser/typescript/phaser.d.ts",
-						  	"app/**/*ts"],
+						  	"app/**/*.ts"],
                     options:{
                         module: "amd",
                         target: "es5"
@@ -28,7 +43,17 @@ module.exports = function(grunt) {
 	                    base: "."
 					}
                 }
-            }
+            },
+			shell: {
+				bowerInstall: {
+					command:  [ "bower install" ].join("&&")
+				},
+				clean:{
+					command:  [	"rm -rf build",
+								"rm -rf bower_components",
+								"rm -rf app/*.js"].join("&&")
+				}
+			},
 		});
 
 	//Plugin loading
@@ -36,7 +61,11 @@ module.exports = function(grunt) {
 	    grunt.loadNpmTasks("grunt-shell");
         grunt.loadNpmTasks("grunt-typescript");
         grunt.loadNpmTasks("grunt-contrib-connect");
+		grunt.loadNpmTasks("grunt-shell");
+		grunt.loadNpmTasks("grunt-contrib-compress");
 
 	//Task definition
-		grunt.registerTask("default", ["typescript:base","connect:server","watch"]);
+		grunt.registerTask("default", ["shell:bowerInstall","typescript:base","connect:server","watch"]);
+		grunt.registerTask("deploy", ["shell:clean","shell:bowerInstall","typescript:base","compress:deploy"]);
+		grunt.registerTask("clean", ["shell:clean"]);
 };
